dc89d7b1ce13889bf3d5f3dfffba1756bc053c6b
Fugo Ohata <s20c1026bg@chibakoudai.jp>
2022-01-12 07:36:17 +0000

    Add a file

--- joykame/src/psjoy.cpp
+++ joykame/src/psjoy.cpp
@@ -1,24 +1,28 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
-void joy_callback(const sensor_msgs::Joy &joy_msg)
+geometry_msgs::Twist cmd_vel;
+void joy_callback(const sensor_msgs::Joy& joy_msg)
 {
-	  // 処理内容を記述
-	     ROS_INFO("axes[0]:%f", joy_msg.axes[0]);   // スティック0の状態を表示 (-1 ～ 1)
-	       ROS_INFO("Button[0]:%d", joy_msg.buttons[0]);  // ボタン0の状態を表示 (0 or 1)
-	       }
-	  
-	       int main(int argc, char **argv)
-	       {
-	         ros::init(argc, argv, "joy_sub_node");
-	           ros::NodeHandle nh;
-	             ros::Subscriber sub = nh.subscribe("joy", 10, joy_callback);
-	               ros::Rate loop_rate(10);
-	  
-	                 while (ros::ok())
-	                   {
-	                       ros::spinOnce();
-	                           loop_rate.sleep();
-	                             }
-	                               return 0;
-	                               }
+	  cmd_vel.linear.x = joy_msg.axes[0];
+	    cmd_vel.linear.y = joy_msg.axes[1];
+	      cmd_vel.angular.z = joy_msg.axes[3];
+}
+
+int main(int argc, char** argv)
+{
+	  ros::init(argc, argv, "psjoy");
+	    ros::NodeHandle nh;
+	      ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+	        ros::Subscriber joy_sub = nh.subscribe("joy", 10, joy_callback);
+
+		  ros::Rate loop_rate(10);
+		    while (ros::ok())
+			      {
+				          cmd_pub.publish(cmd_vel);
+					      ros::spinOnce();
+					          loop_rate.sleep();
+						    }
+		      return 0;
+}
